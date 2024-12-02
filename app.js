09fd49b1f32733504bd3a8f3b18d2ae8d8baff43
Timur <timur.mosenkov@gmail.com>
2024-12-02 20:54:45 +0200

    initialize app

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbUrl = process.env.DB_URL.replace("<db_password>", dbPassword);
+
+// mongoose.set("strictQuery", false);
+// console.log(`Connecting to ${dbUrl}`);
+
+// mongoose
+//   .connect(dbUrl)
+//   .then(() => {
+//     console.log("connected to MongoDB");
+//   })
+//   .catch((error) => console.log("Error connecting to mongo DB", error.message));
+
+// app.use(express.static(path.join(__dirname, "dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
+
+
+module.exports = app;
---
+++ models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  category: String,
+  imageName: String, 
+});
+
+
+productSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Products', productSchema)
