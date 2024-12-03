c0a6db5b2a7f25964aadbd17f71932066fa53ddf
Timur <timur.mosenkov@gmail.com>
2024-12-03 18:35:47 +0200

    working product controller

--- app.js
+++ app.js
@@ -3,32 +3,28 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const productController = require('./controllers/product')
 
 
 app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-// const dbPassword = process.env.DB_PASSWORD;
-// const dbUrl = process.env.DB_URL.replace("<db_password>", dbPassword);
+const dbPassword = process.env.DB_PASSWORD;
+const dbUrl = process.env.DB_URL.replace("<db_password>", dbPassword);
 
 
-// mongoose.set("strictQuery", false);
-// console.log(`Connecting to ${dbUrl}`);
+mongoose.set("strictQuery", false);
+console.log(`Connecting to ${dbUrl}`);
 
-// mongoose
-//   .connect(dbUrl)
-//   .then(() => {
-//     console.log("connected to MongoDB");
-//   })
-//   .catch((error) => console.log("Error connecting to mongo DB", error.message));
-
-// app.use(express.static(path.join(__dirname, "dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => console.log("Error connecting to mongo DB", error.message));
 
 
+app.use('/api/product', productController)
 
 module.exports = app;
--- controllers/product.js
+++ controllers/product.js
@@ -4,4 +4,7 @@ const productSchema = require('../models/productSchema')
 productRouter.post('/', async (req, res) => {
   const product = req.body
   console.log(product)
+  res.json(product)
 })
+
+module.exports = productRouter
